f8eda19cdc65411496235eef967fb4fe21bb6245
MichaelHarrison87 <30554877+MichaelHarrison87@users.noreply.github.com>
2018-08-30 12:28:56 +0000

    Project Submitted

---
+++ python_packages.py
@@ -0,0 +1,11 @@
+import numpy as np
+import pandas as pd
+import tensorflow as tf
+import PIL
+import sklearn
+
+print("Tensorflow:", tf.__version__)
+print("NumPy:", np.__version__)
+print("Pandas:", pd.__version__)
+print("Python Image Library (PIL):", PIL.VERSION)
+print("SciKit-Learn:", sklearn.__version__)
